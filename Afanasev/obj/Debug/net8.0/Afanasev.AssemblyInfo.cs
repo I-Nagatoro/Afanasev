a672bd8f614002b168bdf7e635422137fcc403a0
NikitaOnianov <nikita.onianov@yandex.ru>
2025-11-11 16:49:03 +0300

    init

--- Afanasev/obj/Debug/net8.0/Afanasev.AssemblyInfo.cs
+++ Afanasev/obj/Debug/net8.0/Afanasev.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Afanasev")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede9f72d47bf588a75442f12126fa180fe496abc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a8ce7fdc858befb4b40d7bed48de23269dba29")]
 [assembly: System.Reflection.AssemblyProductAttribute("Afanasev")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Afanasev")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
